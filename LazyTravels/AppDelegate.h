f000718a1f7c1b64b9fa0027aa5e638b303bc9f0
renyida <renyida@dangdang.com>
2018-08-27 14:17:18 +0800

    init project

---
+++ LazyTravels/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LazyTravels
+//
+//  Created by radar on 2018/8/27.
+//  Copyright © 2018年 radar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LazyTravels/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LazyTravels
+//
+//  Created by radar on 2018/8/27.
+//  Copyright © 2018年 radar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
